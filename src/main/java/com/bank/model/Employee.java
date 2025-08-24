383bcd9bd5f68326792a25971503fb0348e834e2
chris Taylor <ctay456a@gmail.com>
2025-08-24 03:29:43 -0400

    Create Employee.java

---
+++ src/main/java/com/bank/model/Employee.java
@@ -0,0 +1,25 @@
+package com.bank.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role; // each employee has one role
+}
