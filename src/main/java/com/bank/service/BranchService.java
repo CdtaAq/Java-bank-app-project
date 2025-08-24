5462b45833ffe276e4d0ac295f87f3fc3337e2cf
chris Taylor <ctay456a@gmail.com>
2025-08-24 02:47:32 -0400

    Create BranchService.java

---
+++ src/main/java/com/bank/service/BranchService.java
@@ -0,0 +1,12 @@
+package com.bank.service;
+
+import com.bank.model.Branch;
+
+import java.util.List;
+
+public interface BranchService {
+    Branch saveBranch(Branch branch);
+    List<Branch> getAllBranches();
+    Branch getBranchById(Long id);
+    void deleteBranch(Long id);
+}
