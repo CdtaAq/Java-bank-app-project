c0fa084fac4e3cb958c80835522213a4bedf2569
chris Taylor <ctay456a@gmail.com>
2025-08-24 03:32:08 -0400

    Create EmployeeServiceImpl.java

---
+++ src/main/java/com/bank/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,36 @@
+package com.bank.service.impl;
+
+import com.bank.model.Employee;
+import com.bank.repository.EmployeeRepository;
+import com.bank.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
