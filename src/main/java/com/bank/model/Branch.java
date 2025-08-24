08443f2ccd14da966c8a37386b595fda5af75b79
chris Taylor <ctay456a@gmail.com>
2025-08-24 02:46:10 -0400

    Create Branch.java

---
+++ src/main/java/com/bank/model/Branch.java
@@ -0,0 +1,24 @@
+package com.bank.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Branch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long branchId;
+
+    @Column(nullable = false, unique = true)
+    private String branchCode;
+
+    @Column(nullable = false)
+    private String branchName;
+
+    private String branchAddress;
+}
