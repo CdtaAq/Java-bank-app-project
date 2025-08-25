8d63acc892602a813f0997bd645c9d4cc2026e0d
chris Taylor <ctay456a@gmail.com>
2025-08-25 17:25:58 -0400

    Update BranchService.java

--- src/main/java/com/bank/service/BranchService.java
+++ src/main/java/com/bank/service/BranchService.java
@@ -1,12 +1,18 @@
 package com.bank.service;
 
 import com.bank.model.Branch;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface BranchService {
-    Branch saveBranch(Branch branch);
-    List<Branch> getAllBranches();
-    Branch getBranchById(Long id);
-    void deleteBranch(Long id);
+    Branch create(Branch branch);
+    Branch update(Long id, Branch branch);
+    void deleteById(Long id);
+
+    Optional<Branch> findById(Long id);
+    Page<Branch> page(Pageable pageable);
+
+    boolean codeExists(String branchCode);
 }
