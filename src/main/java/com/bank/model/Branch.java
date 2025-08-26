ef78d1c0eb70c8c95521037f26833ea5330ff4aa
chris Taylor <ctay456a@gmail.com>
2025-08-26 17:07:24 -0400

    Update Branch.java

--- src/main/java/com/bank/model/Branch.java
+++ src/main/java/com/bank/model/Branch.java
@@ -1,34 +1,25 @@
-
 package com.bank.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
-@Table(
-    name = "branches",
-    uniqueConstraints = @UniqueConstraint(name = "uk_branch_code", columnNames = "branchCode")
-)
-@Getter @Setter @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Branch {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long branchId;
 
-    @NotBlank
-    @Size(min = 3, max = 16)
-    @Column(nullable = false, length = 16)
+    @Column(nullable = false, unique = true)
     private String branchCode;
 
-    @NotBlank
-    @Size(min = 3, max = 64)
-    @Column(nullable = false, length = 64)
+    @Column(nullable = false)
     private String branchName;
 
-    @Size(max = 255)
     private String branchAddress;
 }
