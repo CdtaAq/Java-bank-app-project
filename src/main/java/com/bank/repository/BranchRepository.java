6684ea9a9a60c86b9dace9ef9143211ca3bbfcb4
chris Taylor <ctay456a@gmail.com>
2025-08-26 17:08:09 -0400

    Update BranchRepository.java

--- src/main/java/com/bank/repository/BranchRepository.java
+++ src/main/java/com/bank/repository/BranchRepository.java
@@ -4,5 +4,4 @@ import com.bank.model.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BranchRepository extends JpaRepository<Branch, Long> {
-    boolean existsByBranchCodeIgnoreCase(String branchCode);
 }
