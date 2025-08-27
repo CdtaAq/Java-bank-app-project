8a2ffae1f56738109f1e4721881317b27153ec6c
chris Taylor <ctay456a@gmail.com>
2025-08-27 19:39:57 -0400

    Update UserRepository.java

--- src/main/java/com/bank/repository/UserRepository.java
+++ src/main/java/com/bank/repository/UserRepository.java
@@ -1,4 +1,3 @@
-// SAVE: banking-project/src/main/java/com/bank/repository/UserRepository.java
 package com.bank.repository;
 
 import com.bank.entity.User;
@@ -7,9 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByUsernameIgnoreCase(String username);
-    boolean existsByEmailIgnoreCase(String email);
-
-    Optional<User> findByUsernameIgnoreCase(String username);
-    Optional<User> findByEmailIgnoreCase(String email);
+    boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
