7becc95daf375c123060e4a4f1ff415de46e4de7
chris Taylor <ctay456a@gmail.com>
2025-08-27 17:22:47 -0400

    Update EmployeeController.java

--- src/main/java/com/bank/controller/EmployeeController.java
+++ src/main/java/com/bank/controller/EmployeeController.java
@@ -2,60 +2,59 @@ package com.bank.controller;
 
 import com.bank.model.Employee;
 import com.bank.model.Role;
+import com.bank.model.Branch;
 import com.bank.service.EmployeeService;
 import com.bank.service.RoleService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bank.service.BranchService;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    @Autowired
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
+    private final RoleService roleService;
+    private final BranchService branchService;
 
-    @Autowired
-    private RoleService roleService;
+    public EmployeeController(EmployeeService employeeService, RoleService roleService, BranchService branchService) {
+        this.employeeService = employeeService;
+        this.roleService = roleService;
+        this.branchService = branchService;
+    }
 
-    // Show employee form
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        model.addAttribute("employees", employeeService.getAllEmployees());
+        return "employeeList";
+    }
+
     @GetMapping("/form")
-    public String showEmployeeForm(Model model) {
+    public String showForm(Model model) {
         model.addAttribute("employee", new Employee());
         model.addAttribute("roles", roleService.getAllRoles());
+        model.addAttribute("branches", branchService.getAllBranches());
         return "employeeForm";
     }
 
-    // Save employee
     @PostMapping("/save")
-    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+    public String saveEmployee(@ModelAttribute Employee employee) {
         employeeService.saveEmployee(employee);
         return "redirect:/employees/list";
     }
 
-    // List employees
-    @GetMapping("/list")
-    public String listEmployees(Model model) {
-        List<Employee> employees = employeeService.getAllEmployees();
-        model.addAttribute("employees", employees);
-        return "employeeList";
-    }
-
-    // Edit employee
     @GetMapping("/edit/{id}")
-    public String editEmployee(@PathVariable("id") Long id, Model model) {
-        Employee employee = employeeService.getEmployeeById(id);
-        model.addAttribute("employee", employee);
+    public String editEmployee(@PathVariable Long id, Model model) {
+        model.addAttribute("employee", employeeService.getEmployeeById(id));
         model.addAttribute("roles", roleService.getAllRoles());
+        model.addAttribute("branches", branchService.getAllBranches());
         return "employeeForm";
     }
 
-    // Delete employee
     @GetMapping("/delete/{id}")
-    public String deleteEmployee(@PathVariable("id") Long id) {
+    public String deleteEmployee(@PathVariable Long id) {
         employeeService.deleteEmployee(id);
         return "redirect:/employees/list";
     }
